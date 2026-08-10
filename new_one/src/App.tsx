3cd8c089d8358a33d8ac6e3774601d0c44798932
EkrizdisII <rameshshivu4@gmail.com>
2026-08-10 23:26:24 +0530

    fied routes

--- new_one/src/App.tsx
+++ new_one/src/App.tsx
@@ -3,14 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Sky from "./pages/sky";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sky" element={<Sky />} />
-
       </Routes>
     </BrowserRouter>
   );
